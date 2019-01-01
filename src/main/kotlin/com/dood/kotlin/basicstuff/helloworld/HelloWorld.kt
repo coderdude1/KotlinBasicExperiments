a0c018aba4d8f08de12a525b83b2cfdec963b47b
Randy <drbugsmn@gmail.com>
2018-12-31 23:16:03 -0600

    basic hello world

---
+++ src/main/kotlin/com/dood/kotlin/basicstuff/helloworld/HelloWorld.kt
@@ -0,0 +1,10 @@
+package com.dood.kotlin.basicstuff.helloworld
+
+/**
+ * Notice that these functions (methods) are not members of a class.  functions are first class entities.  note the
+ * lack of semicolons, if you add one intelij warns on it as 'redundant semicolon'
+ */
+
+fun main(args: Array<String>) {
+    println("Hello world Kotlin style!!!")
+}
