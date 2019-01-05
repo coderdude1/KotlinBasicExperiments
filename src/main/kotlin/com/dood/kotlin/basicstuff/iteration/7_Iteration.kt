362c8eba25f105e59f0c82c7b1a8f3b8313b664e
Randy <drbugsmn@gmail.com>
2019-01-04 21:22:17 -0600

    added more range stuff

--- src/main/kotlin/com/dood/kotlin/basicstuff/iteration/7_Iteration.kt
+++ src/main/kotlin/com/dood/kotlin/basicstuff/iteration/7_Iteration.kt
@@ -1,9 +1,14 @@
 package com.dood.kotlin.basicstuff.iteration
 
+import java.util.*
+
 fun main(args: Array<String>) {
     simpleLoop()
     countDownwardEvenOnly()
     countDownToLimit()
+    populateAndIterateOverMap()
+    iterateListWithIndex()
+    rangeMembership()
 }
 
 /**
@@ -13,6 +18,8 @@ fun main(args: Array<String>) {
  *
  * range modifiers can be found here https://kotlinlang.org/docs/reference/ranges.html
  *
+ * Note that there are integral ranges  that w can interate over, IntRange, LongRange, CharRange
+ *
  */
 
 fun fizzBuzz(i: Int) = when {
@@ -20,6 +27,50 @@ fun fizzBuzz(i: Int) = when {
     i % 3 == 3 -> "Fizz\n "
     i % 5 == 0 -> "Buzz\n "
     else -> "$i " //return the input int as a string using stringTemplate
+}
+
+fun recognize(c: Char) = when (c) {
+    in '0'..'9' -> "Its a digit"
+    in 'a'..'z', in 'A'..'Z' -> "Its a letter" //note combining two in statements
+    else -> "no damn clue"
+}
+
+private fun rangeMembership() {
+    println("\nRange Membership")
+    println("3 is letter ${isLetter('3')}")
+    println("z is letter ${isLetter('z')}")
+    println("3 is not digit ${isNotDigit('3')}")
+    println("kotlin" in "java".."scala") //prints true same as "java" <= "kotlin" <= scala case sensitive
+    println("kotlin" in setOf("java", "scala")) //can use in on a collection
+    println("Recognize a ${recognize('a')}")
+    println("Recognize 8 ${recognize('8')}")
+}
+
+private fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+private fun isNotDigit(c: Char) = c !in '0'..'9'
+
+private fun iterateListWithIndex() {
+    println("\niterate list with index")
+    val list = arrayListOf("10", "11", "13", "2") //note no <> type, it uses type inference (future thingy)
+
+    for ((index, element) in list.withIndex()) {
+        println("$index, $element")
+    }
+}
+
+private fun populateAndIterateOverMap() {
+    println("\niterate over TreeMap")
+    val binaryReps = TreeMap<Char, String>() //java treeMap
+
+    for (c in 'A'..'F') { //note we can iterateo over characters
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryReps[c] = binary
+    }
+
+    for ((letter, binary) in binaryReps) { //iterate over the map, letter is key, binary is value
+        println("$letter = $binary")
+    }
+
 }
 
 private fun countDownToLimit() {
