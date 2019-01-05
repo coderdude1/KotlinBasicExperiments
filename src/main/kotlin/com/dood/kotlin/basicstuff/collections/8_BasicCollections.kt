4acd28b885b4d43f90d5cf4c01cca91050c5b061
Randy <drbugsmn@gmail.com>
2019-01-04 23:03:19 -0600

    added comment

--- src/main/kotlin/com/dood/kotlin/basicstuff/collections/8_BasicCollections.kt
+++ src/main/kotlin/com/dood/kotlin/basicstuff/collections/8_BasicCollections.kt
@@ -4,6 +4,9 @@ fun main(args: Array<String>) {
     createBasicCollections()
 }
 
+/**
+ * TODO add mutable vs immutable collections https://kotlinlang.org/docs/reference/collections.html
+ */
 private fun createBasicCollections() {
     val set = hashSetOf("one", "two", "two", "three") //note duplicate "two"
     println("create a set $set")
@@ -15,7 +18,11 @@ private fun createBasicCollections() {
     println("  list is type ${list.javaClass}")
     println("  kotlin adds last to list meaxElement: ${list.last()}")
 
+    list.add("blah")
+    println("added a new element: $list") //note no {}
+
     val map = hashMapOf(1 to "one", 7 to "seven", 33 to "88")
     println("\ncreate map $map")
     println("  map is type ${map.javaClass}")
+
 }
