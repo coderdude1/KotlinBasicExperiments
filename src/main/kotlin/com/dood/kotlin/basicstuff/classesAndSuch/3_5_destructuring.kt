2d117b9c53c82a29486f31c7655b0b607c4ea67f
Randy <drbugsmn@gmail.com>
2019-01-04 23:26:15 -0600

    added TODO for destructing

---
+++ src/main/kotlin/com/dood/kotlin/basicstuff/classesAndSuch/3_5_destructuring.kt
@@ -0,0 +1,10 @@
+package com.dood.kotlin.basicstuff.classesAndSuch
+
+/**
+ * TODO add this might change the prefix
+ * note that these appear to be positional, ie tied to the constructor
+ *
+ * https://kotlinlang.org/docs/reference/multi-declarations.html
+ *
+ * returining Pairs, etc
+ */
