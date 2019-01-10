c3614cf3cf88f5ba5fed14f92ebb34290980ca8c
Randy <drbugsmn@gmail.com>
2019-01-09 20:16:28 -0600

    other stuff

--- src/main/kotlin/com/dood/kotlin/basicstuff/classesAndSuch/13_destructuringInfixPairs.kt
+++ src/main/kotlin/com/dood/kotlin/basicstuff/classesAndSuch/13_destructuringInfixPairs.kt
@@ -6,5 +6,20 @@ package com.dood.kotlin.basicstuff.classesAndSuch
  *
  * https://kotlinlang.org/docs/reference/multi-declarations.html
  *
- * returining Pairs, etc
+ * returining Pairs, infix calls destructuring, spread operator, etc
  */
+
+fun main(args: Array<String>) {
+
+}
+
+private fun infixOperator() {
+    //to is invoking the infix operator.  this is a fun between two values, it returns a Pair
+    // 1 to("one") is same as 1 to "one"
+    val map = mapOf(1 to "one", 7 to "seven", 43 to "forty three")
+
+    //make a function support infix
+    //infix fun Any.to(other: Any) = Pair(this, other) //look at how this maps to above
+
+    val (number, name) = 1 to "one" //destructing to set two vals at once
+}
---
+++ src/main/kotlin/com/dood/kotlin/basicstuff/classesAndSuch/14_MoreInterfaces.kt
@@ -0,0 +1,5 @@
+package com.dood.kotlin.basicstuff.classesAndSuch
+
+interface Clickable {
+    fun click()
+}
