e3662d7b0e5dc4f67ee8145e0d9aa88a1b1cb4c4
Randy <drbugsmn@gmail.com>
2019-03-06 17:52:56 -0600

    some sequence using the when operator

--- src/main/kotlin/com/dood/kotlin/basicstuff/lambdas/24_Sequences.kt
+++ src/main/kotlin/com/dood/kotlin/basicstuff/lambdas/24_Sequences.kt
@@ -1,6 +1,7 @@
 package com.dood.kotlin.basicstuff.lambdas
 
 import com.dood.kotlin.basicstuff.classesAndSuch.UserDataClass
+import java.lang.StringBuilder
 
 /**
  * really nice blog here https://medium.com/@elye.project/kotlin-slow-list-and-lazy-sequence-61691fc974c5
@@ -11,6 +12,7 @@ import com.dood.kotlin.basicstuff.classesAndSuch.UserDataClass
 fun main() {
     veryBasicSequence()
     intermediateOpsAreLazy()
+    whenOperator()
 }
 
 /**
@@ -43,6 +45,9 @@ map (1) map (2) map (3) map (4) filter(1) filter(4) filter(9) filter(16)
 
  * note that all operations are applied for each entry in the sequence, ie the first element is mapped then
  * filtered
+ *
+ * sequences are effectively the same as java 8 streams.  kotlin sequences currently cannot be done parallel
+ * like a java stream
  */
 private fun intermediateOpsAreLazy() {
     println("Usinga a sequence causes Itermediate steps are lazy")
@@ -56,3 +61,51 @@ private fun intermediateOpsAreLazy() {
         .map { print("map ($it) "); it * it }
         .filter { print("filter($it) "); it % 2 == 0 }
 }
+
+/**
+java interface
+void postponeComutation(int delay, Runnable computation);
+
+Kotlin impl with lambda
+postponeComputation(1000) {println 42}
+
+Another option
+postponeCompuation(100, Object : Runnable {
+override fun run() {
+println(42)
+}
+};
+ */
+private fun whenOperator() {
+    println("when operator")
+    println("alphabet: ${alphabet()}")
+    println("alphabet: ${alphabetWithWhen()}")
+}
+
+//when operator
+private fun alphabet(): String {
+    println("alphabet withouth the when operator")
+    val result = StringBuilder()
+    for (letter in 'A'..'Z') {
+        result.append(letter)
+    }
+    result.append("\n now I knw the alphabet")
+    return result.toString()
+}
+
+//with is a function, not a language thingy
+//intellij annotates things with 'this: StringBuilder and 'with toString'
+private fun alphabetWithWhen(): String {
+    println("alphabet with when operator")
+    val stringBuilder = StringBuilder()
+    //with specifies the recierver (putting this comment after the next makes the intellij ghost text go away
+    return with(stringBuilder) {
+        for (letter in 'A'..'Z') {
+            this.append(letter) //this is optional, but points to the receriver (stringBuilder)
+        }
+        append("now I know the alphabet")
+        toString() //returnthe recierver.toString
+    }
+}
+
+
